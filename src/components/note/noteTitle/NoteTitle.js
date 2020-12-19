e9227b8303b352fdfac8a71b2043d5c9c5df3064
Sergei Kostrikin <cptlbc@yandex.ru>
2020-12-18 21:01:32 -0500

    Border design small change

--- src/components/note/noteTitle/NoteTitle.js
+++ src/components/note/noteTitle/NoteTitle.js
@@ -1,9 +1,13 @@
 import { NoteHeader } from "./NoteTitleStyles";
 
-const NoteTitle = ({note: { title, id }, onChange }) => {
+const NoteTitle = ({ note: { title, id }, onChange }) => {
   return (
-    <NoteHeader onChange={(e) => onChange(e, false, id)} value={title} maxLength="20"/>
-  )
-}
+    <NoteHeader
+      onChange={(e) => onChange(e, false, id)}
+      value={title}
+      maxLength="20"
+    />
+  );
+};
 
-export default NoteTitle
+export default NoteTitle;
