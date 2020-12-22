493f8e51287b20af1f1c21facdb13e06f34ba1c7
Sergei Kostrikin <cptlbc@yandex.ru>
2020-12-21 20:41:02 -0500

    Save(react-beautiful-dnd works not correctly)

--- src/components/note/NoteList.js
+++ src/components/note/NoteList.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-  
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import { NotesContainer, NoteCard } from "./NoteStyles";
 import NoteTitle from "./noteTitle/NoteTitle";
 import Quill from "./quill/Quill";
@@ -50,27 +51,50 @@ const NoteList = () => {
     } else return;
   };
 
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const notesCopy = Array.from(notes);
+    const [reorderedItem] = notesCopy.splice(result.source.index, 1);
+    notesCopy.splice(result.destination.index, 0, reorderedItem);
+
+    setNotes(notesCopy);
+  };
+
   return (
-    <>
-      <NotesContainer>
-        {notes.map((note) => (
-          <NoteCard key={note.id}>
-            <NoteTitle
-              note={note}
-              onChange={handleChange}
-              value={"title"}
-              maxLength="20"
-            />
-            <Quill
-              value={note.text}
-              id={note.id}
-              onChange={handleChange}
-              onDelete={handleDelete}
-            />
-          </NoteCard>
-        ))}
-      </NotesContainer>
-    </>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="characters">
+        {(provided) => (
+          <NotesContainer {...provided.droppableProps} ref={provided.innerRef}>
+            {notes.map((note, index) => (
+              <Draggable key={note.id} draggableId={note.id} index={index}>
+                {(provided) => (
+                  <NoteCard
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <NoteTitle
+                      note={note}
+                      onChange={handleChange}
+                      value={"title"}
+                      maxLength="20"
+                    />
+                    <Quill
+                      value={note.text}
+                      id={note.id}
+                      onChange={handleChange}
+                      onDelete={handleDelete}
+                    />
+                  </NoteCard>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </NotesContainer>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
