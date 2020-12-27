513afd17286446029ada0b3cdf34124dc5cfc52c
Sergei Kostrikin <cptlbc@yandex.ru>
2020-12-26 21:03:09 -0500

    Bug fix about  mising state after edit notes page reload

--- src/App.js
+++ src/App.js
@@ -5,35 +5,34 @@ import NotFound from "./pages/notFound";
 import NotesCategories from "./pages/notesCategories";
 import EditNote from "./pages/editNote";
 
-function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: "42d14e98-3021-4841-bf41-2d550e79c76c",
-      category: "How to use this app",
-      text:
-        "<h1>How to use this notes</h1> <p>Your notes will be store in your browser in the local storage. If you clear local storage your data will be lost.</p><h2>Hot keys:</h2><ul><li>Ctrl + b -> bold text</li><li>Ctrl + u -> underline text</li><li>Ctrl + z -> undo</li><li>Ctrl + z -> undo</li><li>Ctrl + y -> redo</li></ul>",
-    },
-    {
-      id: "e33903ac-9212-4595-8858-916704557c37",
-      category: "Grocery list",
-      text: "Milk",
-    },
-    {
-      id: "fa26200b-1486-4930-9e6a-29c6a5c5d867",
-      category: "Grocery list",
-      text: "Bread",
-    },
-    {
-      id: "e4a8c90d-45ed-443c-9572-8201c1e4899b",
-      category: "Grocery list",
-      text: "Apples",
-    },
-  ]);
+const initialState = [
+  {
+    id: "42d14e98-3021-4841-bf41-2d550e79c76c",
+    category: "How to use this app",
+    text:
+      "<h1>How to use this notes</h1> <p>Your notes will be store in your browser in the local storage. If you clear local storage your data will be lost.</p><h2>Hot keys:</h2><ul><li>Ctrl + b -> bold text</li><li>Ctrl + u -> underline text</li><li>Ctrl + z -> undo</li><li>Ctrl + z -> undo</li><li>Ctrl + y -> redo</li></ul>",
+  },
+  {
+    id: "e33903ac-9212-4595-8858-916704557c37",
+    category: "Todo",
+    text: "Learn TypeScript",
+  },
+  {
+    id: "fa26200b-1486-4930-9e6a-29c6a5c5d867",
+    category: "Todo",
+    text: "Learn React Native",
+  },
+  {
+    id: "e4a8c90d-45ed-443c-9572-8201c1e4899b",
+    category: "Todo",
+    text: "Learn Next.js",
+  },
+];
 
-  useEffect(() => {
-    const notes = JSON.parse(localStorage.getItem("userInput"));
-    if (notes) setNotes(JSON.parse(localStorage.getItem("userInput")));
-  }, []);
+function App() {
+  const [notes, setNotes] = useState(() => {
+    return JSON.parse(localStorage.getItem("userInput")) || initialState;
+  });
 
   useEffect(() => {
     localStorage.setItem("userInput", JSON.stringify(notes));
