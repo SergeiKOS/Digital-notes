0e1b293762041520d5de5d6085209d1697396831
Sergei Kostrikin <cptlbc@yandex.ru>
2021-01-02 23:56:37 -0500

    Save

---
+++ src/components/modal/index.js
@@ -0,0 +1,12 @@
+const Modal = ({ message, isOpen, onClose }) => {
+  if (!isOpen) return null;
+  return ReactDOM.createPortal(
+    <div className="modal">
+      <span className="message">{message}</span>
+      <button onClick={onClose}>Close</button>
+    </div>,
+    domNode
+  );
+};
+
+export default Modal;
