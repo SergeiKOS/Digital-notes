27e96b16f591d76eb2e9dc78d17dbe7619f138a4
Sergei Kostrikin <cptlbc@yandex.ru>
2021-01-12 10:30:28 -0500

    Sync notFound

---
+++ src/pages/notFound/index.js
@@ -0,0 +1,18 @@
+import { NotFoundWrapper } from "./NotFoundStyles";
+import Header from "../../components/header";
+import { ReactComponent as NotFoundSVG } from "./not-found.svg";
+import GoHome from "./GoHomeBtn";
+
+const NotFound = () => {
+  return (
+    <>
+      <Header />
+      <NotFoundWrapper>
+        <NotFoundSVG style={{ width: "100%" }} />
+        <GoHome />
+      </NotFoundWrapper>
+    </>
+  );
+};
+
+export default NotFound;
