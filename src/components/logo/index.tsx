d7b89017007f7efffaa2a9b6c9273cc7aab83203
Sergei Kostrikin <s.a.kostrikin@gmail.com>
2021-03-09 16:53:21 -0500

    Add logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,11 +1,18 @@
 import ILogo from "./ILogo.interface";
 import { LogoType } from "./LogoStyles";
 import { Link } from "react-router-dom";
+import { ReactComponent as Logotype } from "./dn-logo.svg";
 
 const Logo = ({ mainPage }: ILogo) => {
   return (
     <LogoType aria-label="Digital notes logo">
-      {mainPage ? "DN" : <Link to="/">DN</Link>}
+      {mainPage ? (
+        <Logotype />
+      ) : (
+        <Link to="/">
+          <Logotype />
+        </Link>
+      )}
     </LogoType>
   );
 };
